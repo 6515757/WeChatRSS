fb7652389b251a2c862abed895095fc686f1b645
qifeichen <651812267@qq.com>
2026-05-07 17:28:06 +0800

    fix: 完善 sql.js 类型声明

--- wechat-rss-analyzer/src/types.d.ts
+++ wechat-rss-analyzer/src/types.d.ts
@@ -1,1 +1,15 @@
-declare module 'sql.js';
+declare module 'sql.js' {
+  export interface Database {
+    run(sql: string, params?: any[]): void;
+    exec(sql: string): any[];
+    export(): Uint8Array;
+    close(): void;
+  }
+
+  interface SqlJsStatic {
+    Database: new (data?: ArrayLike<number> | Buffer | null) => Database;
+  }
+
+  export default function initSqlJs(config?: any): Promise<SqlJsStatic>;
+  export { Database };
+}
